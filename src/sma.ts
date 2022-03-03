806b817fd4460f3840243d6190a944a6eb3eb3eb
swiing <persovince@yahoo.fr>
2022-03-03 21:49:06 +0100

    refactor: avoid test coverage being confused
    See https://github.com/kulshekhar/ts-jest/issues/332 for rational

--- src/sma.ts
+++ src/sma.ts
@@ -21,7 +21,8 @@ export default function* sma(data: Float64Array, period: number) {
 
   // code coverage will be confused here - https://github.com/kulshekhar/ts-jest/issues/332
   /* istanbul ignore next */ // does not work
-  while (true) {
+  // while (true) {
+  for (;;) {
     yield ma / period;
     if (i >= len) return;
     ma += data[i] - data[i - period];
