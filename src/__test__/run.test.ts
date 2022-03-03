a1687b61f095f38acf726806fe34da43a9d5c27b
swiing <persovince@yahoo.fr>
2022-03-03 21:56:06 +0100

    refactor: create a test utility to make it super simple to add tests

---
+++ src/__test__/run.test.ts
@@ -0,0 +1,56 @@
+/**
+ * This is the entry point to run all tests
+ */
+
+// run through all .ts files under ./src,
+// check for a matching .test.json test file
+// and call the test.
+// exit(1) if no test file found, hence enforcing a test file for each indicator
+
+/* eslint-disable no-console */
+import fs from 'fs';
+import path from 'path';
+
+import unittest from './util';
+
+fs.readdirSync('./src')
+  .filter((file) => path.extname(file) === '.ts')
+  .forEach((file) => {
+    const fn = file.split('.')[0];
+
+    try {
+      // if there is no matching test file, this will throw,
+      // and this is expected.
+      const fileData = fs.readFileSync(path.join('./src', `${fn}.test.json`));
+
+      // The json test file is expected to provide 1 or more (i.e. an array of) tests
+      Array.from(JSON.parse(fileData.toString())).forEach((value) => {
+        // @ts-ignore Property '...' does not exist on type '{}'
+        const { desc, input, param, output = false } = value;
+
+        // check the json test file provides the expected info
+        // desc and output are actually optional. input and param are mandatory
+        if (!Array.isArray(input) || Number.isNaN(param))
+          throw new SyntaxError();
+
+        // all looks good. Run actual test!
+        unittest({ fn, desc, input, param, output });
+      });
+    } catch (err) {
+      // from typescript 4.4(?) err is considered of type unknown by default in catch
+      // This leads to typescript complaining about err.code, so I cast err
+      // However this leads to another issue that NodeJS is not defined.
+      // Solved by https://github.com/Chatie/eslint-config/issues/45#issuecomment-885507652
+      // (@TODO: this seems a temporary solution though)
+      if ((err as NodeJS.ErrnoException).code === 'ENOENT')
+        console.error(
+          `You MUST provide a test file ${fn}.test.json in ./src for ${fn}.`
+        );
+      else if (err instanceof SyntaxError)
+        console.error(`Syntax error in ${fn}.test.json`);
+
+      // non-0 exit in case this is used in a CI pipeline
+      // (e.g. want github action to fail)
+      process.exit(1);
+    }
+  });
---
+++ src/__test__/util.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+
+type testargs = {
+  // e.g. "sma" // (input: Float64Array, param: number) => Generator<number, void, unknown>;
+  fn: string;
+  // e.g. "simle moving average"
+  // will default to value of fn if not provided
+  desc?: string;
+  // e.g. [10,11,12,13]
+  input: number[];
+  // e.g. period = 3
+  param: number;
+  // the expected result of the computation by the indicator
+  // falsy if computation is expected to fail (throw)
+  output?: number[] | false;
+};
+
+export default function test({ fn, desc, input, param, output }: testargs) {
+  describe(`${fn}(...)`, () => {
+    it(`should ${!output ? `throw${desc ? ' in case of' : ''}` : 'yield'} ${
+      desc || fn
+    }`, async () => {
+      const { default: fct } = await import(path.join('../', fn));
+      const generator = fct(new Float64Array(input), param);
+
+      if (!output) expect(generator.next).toThrow();
+      else {
+        while (output.length)
+          expect(generator.next().value).toBe(output.shift());
+        expect(generator.next().done).toBe(true);
+      }
+    });
+  });
+}
