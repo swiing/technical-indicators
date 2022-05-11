05c188fa523178f225cd894319f61111e3f65858
swiing <persovince@yahoo.fr>
2022-05-11 19:44:07 +0200

    feat: add variation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as rsi } from './rsi.js';
 export { default as sma } from './sma.js';
 export { default as stddev } from './stddev.js';
+export { default as variation } from './variation.js';
